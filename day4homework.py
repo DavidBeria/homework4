b4b58e795fe98ddfd89c934799a68982a1e629b5
DavidBeria <143294896+DavidBeria@users.noreply.github.com>
2023-10-05 21:36:15 +0400

    Add files via upload

---
+++ day4homework.py
@@ -0,0 +1,18 @@
+name = input("enter your name: ")
+print("your name is ", name)
+
+age = input("enter your age: ")
+print("your age is ", age)
+
+surname = input("enter your surname: ")
+print("your surname is ", surname)
+
+MomAge = input("enter your moms name: ")
+print("your moms name is ", MomAge)
+
+DadAge = input("enter your dads name: ")
+print("your dads name is ", DadAge)
+
+future = int(age) + 15
+
+print("In 15 years you will be", future)
